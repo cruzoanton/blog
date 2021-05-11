337dfc22d0ffcda76f24e74dad173b8b9dfe52bf
anton <cruzoanton@gmail.com>
2021-05-11 12:13:55 +0300

    not work(

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+  include Visible
+  belongs_to :user
+
+    has_many :comments, dependent: :destroy
+    has_many :commenters, through: :comments, source: :user
+    has_many :unique_commenters, -> { distinct }, through: :comments, source: :user
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  end
